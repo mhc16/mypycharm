ce363149aacfd39f9e836293a459f98005888cd1
huma <mafengming2020@gmail.com>
2021-10-22 01:47:59 -0700

    test

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import collections
+import datetime
+import queue
+from collections import defaultdict
+import heapq
+from typing import List
+import time
+import threading
+a = [1,2,3]
+del a[1]
+print(a)
